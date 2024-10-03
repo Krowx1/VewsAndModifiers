02102c7e041aaa9629c043112d567958d7ee4252
Krow <katie.scantland@stu.glasgow.kyschools.us>
2024-10-03 08:42:47 -0500

    Initial Commit

---
+++ VewsAndModifiers/VewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  VewsAndModifiersApp.swift
+//  VewsAndModifiers
+//
+//  Created by Krow on 10/3/24.
+//
+
+import SwiftUI
+
+@main
+struct VewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
